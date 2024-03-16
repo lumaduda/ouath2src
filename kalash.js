a4ae75e73179edddbc45cbdcec5cc193c2db37e3
lumaduda <65085286+lumaduda@users.noreply.github.com>
2024-03-16 00:04:52 -0400

    Add files via upload

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "2020",
+  prefix: ">",
+  client: "MTIxODMwOTI5NjI2NjI4NTA2Nw.GIsXh9.egrwSWRVZlSR9x8pJsTklPwSq_Cf6hhaHXmsXY",
+  client_id: "1218309296266285067",
+  client_secret: "6HRLpWzfSj66kQvPxnxH9bK6T6eNeoro",
+  redirect_uri: "http://45.158.76.19:2020/",
+  footer: "",
+  support: "",
+  wehbook: "https://discord.com/api/webhooks/1218133216867651654/OkwDLEakz27NCBNUMNcfHBkPTypB1CkqPeRucXVEfBU2CsG7jFoEWJk4q1jfnnyyHIUJ", 
+  owners: ["1180172439410454588"],
+  authLink: `https://discord.com/oauth2/authorize?client_id=1218309296266285067&response_type=code&redirect_uri=http%3A%2F%2F45.158.76.19%3A2020%2F&scope=identify+guilds+guilds.join`,
+
+}
